487a6d8326a65bdb256a3eff6fbe14daeef1d92a
Adam <adhocadam@protonmail.com>
2023-02-05 11:41:02 -0600

    initial commit

---
+++ CareLink.psm1
@@ -0,0 +1,206 @@
+<#
+.SYNOPSIS
+A PowerShell Module to interact with Minimed CareLink
+.DESCRIPTION
+This PowerShell module emulates browser interaction to Minimed CareLink to retrieve a patient's blood glucose data
+#>
+
+function Get-CareLinkToken {
+    param (
+        #The Carelink username
+        [parameter(ParameterSetName = 'ManualCred', Mandatory = $true, Position = 0)]
+        [string]$username,
+        #The Carelink password
+        [parameter(ParameterSetName = 'ManualCred', Mandatory = $true, Position = 1)]
+        [string]$password,
+        #PowerShell credential object that contains username/password
+        [parameter(ParameterSetName = 'PSCredential', Mandatory = $true, Position = 2)]
+        [PSCredential]$Credential,
+        #Browser to impersonate,
+        [parameter(Mandatory = $false, Position = 3)]
+        [ValidateSet("InternetExplorer", "Firefox", "Chrome", "Opera", "Safari")]
+        [string]$BrowserName="Chrome"
+    )
+
+    #login page
+    $loginPage = "https://carelink.minimed.com/patient/sso/login?country=us&lang=en"
+    $login = Invoke-WebRequest -Uri $loginPage -UserAgent $userAgent -UseBasicParsing -SessionVariable medtronic
+    $sessionID = ($login.InputFields | Where-Object { $_.name -eq "sessionID" }).Value
+    $sessionDataOne = ($login.InputFields | Where-Object { $_.name -eq "sessionData" }).Value
+
+    #define the user agent string
+    $userAgentList = [Microsoft.PowerShell.Commands.PSUserAgent].GetProperties() | Select-Object Name, @{Name = 'UserAgent'; Expression = { [Microsoft.PowerShell.Commands.PSUserAgent]::$($_.Name) } }
+    $userAgent = $userAgentList | Where-Object { $_.Name -eq $BrowserName }
+
+    #if the credentials came in via PSCredentialObject, copy them to update variable names to be used later
+    if ($PsCmdlet.ParameterSetName -eq "PSCredential")
+    {
+        $username = $credential.UserName
+        $password = $credential.GetNetworkCredential().Password
+    }
+
+    #login to carelink
+    $loginBody = @{
+        "sessionID"    = $sessionID
+        "sessionData"  = $sessionDataOne
+        "locale"       = "en"
+        "action"       = "login"
+        "actionButton" = "Log in"
+        "username"     = $username
+        "password"     = $password
+    }
+    $loginResponse = Invoke-WebRequest -Uri "https://mdtlogin.medtronic.com/mmcl/auth/oauth/v2/authorize/login" -Body $loginBody -Method "POST" -UserAgent $userAgent -ContentType "application/x-www-form-urlencoded" -UseBasicParsing -WebSession $medtronic
+
+    #since we're using basic parsing in the above call to avoid the Internet Explorer dependency, we need to convert the response's Content to HTML to fish out the sessionData value
+    $loginResponseHTML = New-Object -comobject "HTMLFile"
+    $loginResponseBytes = [System.Text.Encoding]::Unicode.GetBytes($loginResponse.content)
+    $loginResponseHTML.write($loginResponseBytes)
+    $sessionDataTwo = $loginResponseHTML.getElementById("sessionData").value
+
+    #consent, use the sessionId and sessionData from the login response
+    $consentBody = @{
+        "action"        = "consent"
+        "response_mode" = "query"
+        "response_type" = "code"
+        "sessionID"     = $sessionId
+        "sessionData"   = $sessionDataTwo
+    }
+    $consentPageResponse = Invoke-WebRequest -Uri "https://mdtlogin.medtronic.com/mmcl/auth/oauth/v2/authorize/consent" -Body $consentBody -Method "POST" -UserAgent $userAgent -ContentType "application/x-www-form-urlencoded" -UseBasicParsing -WebSession $medtronic
+
+    #get the auth token and its expiration date from the cookies in the $medtronic websession
+    $authTmpToken = $medtronic.cookies.GetCookies("https://carelink.minimed.com") | Where-Object { $_.Name -eq "auth_tmp_token" }
+    $cTokenValidTo = $medtronic.cookies.GetCookies("https://carelink.minimed.com") | Where-Object { $_.Name -eq "c_token_valid_to" }
+
+    #turn into a PSCustomObject for subsequent use
+    $token = [PSCustomObject] @{
+        Token           = $authTmpToken
+        TokenExpiration = $cTokenValidTo
+        Websession      = $medtronic
+        UserAgent       = $userAgent
+        username        = $username
+        password        = $password
+    }
+
+    return $token
+}
+
+#retrieve user account information such as their Login Date, Account ID, and User Role
+function Get-CareLinkAccount {
+    param (
+        #The Carelink username
+        [parameter(Mandatory = $true, Position = 0)]
+        [PSCustomObject]$Token
+    )
+
+    #verify the token is still valid before proceding
+    $token = Confirm-CarelinkToken -Token $token
+
+    $authHeader = @{
+        "Accept"          = "application/json, text/plain, */*"
+        "Accept-Encoding" = "gzip, deflate, br"
+        "Accept-Language" = "en-US,en;q=0.6"
+        "Authorization"   = "Bearer $($Token.Token.value)"
+        "Referer"         = "https://carelink.minimed.com/app/home"
+        "Sec-Fetch-Dest"  = "empty"
+        "Sec-Fetch-Mode"  = "cors"
+        "Sec-Fetch-Site"  = "same-origin"
+        "Sec-GPC"         = "1"
+      }
+
+    #call the Me rest endpoint
+    $me = Invoke-RestMethod -Uri "https://carelink.minimed.com/patient/users/me" -Method "GET" -header $authHeader -UserAgent $token.userAgent -WebSession $token.websession
+    return $me
+}
+
+#retrieve user profile information, username, phone number, etc.
+function Get-CareLinkProfile {
+    param (
+        #The Carelink username
+        [parameter(Mandatory = $true, Position = 0)]
+        [PSCustomObject]$Token
+    )
+
+    #verify the token is still valid before proceding
+    $token = Confirm-CarelinkToken -Token $token
+
+    $authHeader = @{
+        "Accept"          = "application/json, text/plain, */*"
+        "Accept-Encoding" = "gzip, deflate, br"
+        "Accept-Language" = "en-US,en;q=0.6"
+        "Authorization"   = "Bearer $($Token.Token.value)"
+        "Referer"         = "https://carelink.minimed.com/app/home"
+        "Sec-Fetch-Dest"  = "empty"
+        "Sec-Fetch-Mode"  = "cors"
+        "Sec-Fetch-Site"  = "same-origin"
+        "Sec-GPC"         = "1"
+      }
+
+    #call the Me rest endpoint
+    $me = Invoke-RestMethod -Uri "https://carelink.minimed.com/patient/users/me/profile" -Method "GET" -header $authHeader -UserAgent $token.userAgent -WebSession $token.websession
+    return $me
+}
+
+function Get-CareLinkData {
+    param (
+        #The Carelink username
+        [parameter(Mandatory = $true, Position = 0)]
+        [PSCustomObject]$Token,
+        #The Carelink Account User
+        [parameter(Mandatory = $true, Position = 1)]
+        [PSCustomObject]$CarelinkUserProfile,
+        #The Carelink User's Profile
+        [parameter(Mandatory = $true, Position = 2)]
+        [PSCustomObject]$CarelinkUserAccount
+    )
+
+    #verify the token is still valid before proceding
+    $token = Confirm-CarelinkToken -Token $token
+
+    #authentication header to make the request
+    $authHeader = @{
+        "Accept"          = "application/json, text/plain, */*"
+        "Accept-Encoding" = "gzip, deflate, br"
+        "Accept-Language" = "en-US,en;q=0.6"
+        "Authorization"   = "Bearer $($Token.Token.value)"
+        "Referer"         = "https://carelink.minimed.com/app/home"
+        "Sec-Fetch-Dest"  = "empty"
+        "Sec-Fetch-Mode"  = "cors"
+        "Sec-Fetch-Site"  = "same-origin"
+        "Sec-GPC"         = "1"
+      }
+
+    #pump, sensor, and glucose
+    $payload = @{
+        "username" = $CarelinkUserProfile.username
+        "role"     = $CarelinkUserAccount.role
+    } | ConvertTo-Json
+
+  $data = Invoke-RestMethod -Uri "https://clcloud.minimed.com/connect/v2/display/message" -Method "POST" -body $payload -header $authHeader -UserAgent $token.userAgent -WebSession $token.websession
+  return $data
+}
+
+function Confirm-CareLinkToken {
+    param (
+        #The Carelink token to validate
+        [parameter(Mandatory = $true, Position = 0)]
+        [PSCustomObject]$Token
+    )
+
+    #convert the token expiration string to a datetime object to compare
+    #string/datetime conversion, https://learn.microsoft.com/en-us/previous-versions/windows/it-pro/windows-powershell-1.0/ee692801(v=technet.10)?redirectedfrom=MSDN
+    $expiration = [datetime]::ParseExact($token.TokenExpiration.Value.Split("UTC")[0].Trim(), "ddd MMM dd HH:mm:ss", $null)
+    $nowUTC = (Get-Date).ToUniversalTime()
+
+    #compare the expiration date to now
+    if ($nowUTC -gt $expiration)
+    {
+        Write-Output "Token expired. Renewing..."
+        $newToken = Get-CareLinkToken -username $token.username -password $token.password
+        return $newToken
+    }
+    else
+    {
+        Write-Output "Token valid"
+        return $token
+    }
+}
---
+++ README.md
@@ -0,0 +1,120 @@
+<h1 align="center">
+  <br>
+  <img width="400" src="https://user-images.githubusercontent.com/6636040/216787966-a0d875c5-dcbe-4eba-849b-79229768c4b2.png">
+  <br>
+    CareLink for PowerShell
+  <br>
+</h1>
+
+## Overview
+
+This PowerShell module is for diabetics looking to retrieve their blood glucose readings from Medtronic Carelink. This is achieved using PowerShell's native [Invoke-WebRequest](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/invoke-webrequest?view=powershell-7.3) and [Invoke-RestMethod](https://learn.microsoft.com/en-us/powershell/module/Microsoft.PowerShell.Utility/Invoke-RestMethod?view=powershell-5.1)'s cmdlets.
+
+This module is compatible with PowerShell 5.1 and up. It has been tested and confirmed working with a 770g. Pull Requests are welcome!
+
+## Available cmdlets
+
+| Cmdlet                |    Purpose    |
+| --------------------- | ------------- |
+| Get-CareLinkToken     | Authenticates to Carelink using your username/password  |
+| Confirm-CareLink      | Used by other functions to confirm your authentication to Carelink is still valid |
+| Get-CareLinkAccount   | Retrieves information about your account such as login date, account id, and user role |
+| Get-CareLinkProfile   | Retrieves information about your profile such as username, phone number, email, etc. |
+| Get-CareLinkData      | Retrieves a detailed object that contains device information, serial number, reservoir levels, last sugar, last 24 hours of sugars, etc. |
+
+## Getting Started
+
+Since the CareLink data model or command line may not be the most intuitive thing to most users. It's suggested to use either PowerShell ISE (built into Windows) or [VSCode](https://code.visualstudio.com/) to build scripts, debug, and visualize information. For the purposes of demonstration below, examples will take place in VSCode.
+
+Install from the module from the PowerShell Gallery by using the following at a PowerShell prompt
+
+```powershell
+Install-Module -Name CareLink
+```
+
+## Authenticate to CareLink
+
+This cmdlet can be used one of two ways to sign into CareLink.
+1. Enter your username and password as values to the Get-CareLinkToken cmdlet. Then store this in a variable, for example called $token.
+
+```powershell
+$token = Get-CareLinkToken -username "???????" -password "????????"
+```
+It should be noted that entering credentials in plaintext within a script 100% violates PowerShell best practices. To curb this, it's recommended to use the PowerShell Secrets module to safely store and consume credentials on your computer. Installing and use is simple:
+```powershell
+#install the module, only need to do this once
+Install-Module -Name Microsoft.PowerShell.SecretManagement -Repository PSGallery
+#store the password. You can run the identical cmdlet every time you ever want to update the value
+Set-Secret -Name "CarelinkPW"
+
+#consume the secret
+$carelinkpassword = Get-Secret -Name "CarelinkPW" -AsPlainText
+$token = Get-CareLinkToken -username "???????" -password "$carelinkpassword"
+```
+
+or
+
+2. Use of a PSCredential. This will prompt you to enter credentials everytime.
+```powershell
+$creds = Get-Credential
+$token = Get-CareLinkToken -credential $creds
+```
+
+## Get account/profile details
+
+Once you have a token, use it with subsequent cmdlets to retrieve more information such as your Account and Profile information. You'll ultimately need these to access your sugar data.
+
+```powershell
+$account = Get-CarelinkAccount -token $token
+$userProfile = Get-CarelinkProfile -token $token
+```
+
+## Retrieve last sugar, last 24 hours of sugar, device serial number, etc.
+
+Using the $token, $account, and $userProfile variables. Pass them as values to the Get-CareLinkData cmdlet to retrieve pertinent information. Save the entire object to a variable such as $data to explore.
+
+```powershell
+$data = Get-CarelinkData -Token $token -CarelinkUserAccount $account -CarelinkUserProfile $userProfile
+```
+
+## PowerShell ISE/VSCode exploration
+
+This is where it makes sense to have performed the above commands in one of these two programs as you can just open a new tab to explore the object without continuing to make repeated calls to CareLink. Take the following gif wherein all the commands are executed in a window, then a new window is opened just to explore the $data variable.
+
+![loadData01](https://user-images.githubusercontent.com/6636040/216782943-6a18ab1e-349f-4bd7-a50e-61c261c1bdf2.gif)
+
+In VSCode you either use CTRL+N to open a new tab/file, or just head over to File -> New File. Set the language to PowerShell, then hit F5 to run it.
+
+![image](https://user-images.githubusercontent.com/6636040/216783020-cc6de797-0430-48c8-978d-f1891b4a2ed7.png)
+
+If you want to interrogate a specific data point, just add a "." to the end of data to grab specific data points. For example:
+- $data.sgs
+- $data.timeFormat
+
+or if you prefer, you can also use the PowerShell pipeline:
+```powershell
+$data | Select-Object sgs, timeFormat, markers
+```
+
+## Filter the sugars
+
+If you were to use $data.sgs, you'd return a list of sugar objects from oldest to most recent. Using PowerShell, we can filter this down such as the last 10 readings, most recent reading, readings with a range, etc.
+
+## The last reading(s)
+```powershell
+$data.sgs | Select-Object -Last 1
+$data.sgs | Select-Object -Last 10
+```
+
+## All readings above (greater than) 160
+```powershell
+$data.sgs | Where-Object {$_.sg -gt 160}
+```
+
+## Readings between a given range
+```powershell
+$data.sgs | Where-Object {($_.sg -ge 90) -and ($_.sg -le 160)}
+```
+
+# Disclaimer
+This project and subsequent PowerShell Module is not associated, affiliated, endorsed, or supported in any capacity by Medtronic or Microsoft. Use of this module is undertaken entirely at your own risk.
